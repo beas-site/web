8a6d143f3a1114936cd2683c36c452477dd07487
beas-site <58391367+beas-site@users.noreply.github.com>
2020-06-25 08:22:23 +0200

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,46 @@
+/* Declare and initialize variables */
+// var Nameofthevariable = value of the variable
+//for instance var circleX = 50;
+var circleX = 0;
+
+/* Set up the Canvas */
+function setup() {
+  createCanvas(400, 400);
+  
+  /* Color the background once */
+  background(200, 0, 220);
+}
+
+/* Set up the draw function over and over */
+function draw() {
+  /* Color the background over and over */
+  //background(R,G,B);
+  
+  /* Draw a line on the background */
+  line(40, 0, 400, 340);
+  
+  /* Draw a rectangle on the line in the middle of the Canvas */
+  rectMode(CENTER);
+  
+  /* Fill the interior of the rectangle */
+  fill(0, 0, 255);
+  
+  /* Color the outline of the rectangle */
+  stroke(0, 255, 0);
+  strokeWeight(8);
+  
+  /* Draw the rectangle */
+  rect(200, 150, 150, 150);
+  
+  /*Draw a ellipse*/
+  fill(random(0, 255), random(0, 255), random(0 ,255));
+  noStroke();
+  ellipse(circleX, 200, 25, 25);
+  
+  circleX= circleX + 1;
+}
+
+/* Set up an event function, happens only if the user pressed the mouse */
+function mousePressed() {
+  background(200, 0, 220);
+}
