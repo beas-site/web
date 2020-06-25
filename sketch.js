08d350b23804efa2ce793c821976d93cd83f0a00
beas-site <58391367+beas-site@users.noreply.github.com>
2020-06-25 11:48:42 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -12,9 +12,9 @@ function setup() {
   
   /* Color the background once */
   bgcolor = color(200, 0, 220);
-  
-  h1 = createElement('h1', 'Als je op de muis klikt verandert deze tekst en de kleur van de canvas');
   createButton("Klik hier");
+  h1 = createElement('h1', 'Als je op de muis klikt verandert deze tekst en de kleur van de canvas');
+  
   
 }
 
