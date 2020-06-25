e77e5dff5722b7e2ddadaad277ba3c984b03a8b6
beas-site <58391367+beas-site@users.noreply.github.com>
2020-06-25 13:42:36 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -16,7 +16,7 @@ function setup() {
   createP("");
   button = createButton("Klik hier om de kleur te veranderen.");
   button.mousePressed(changeColor);
-  createP("");
+  
   input = createInput ('type hier je naam');
   h1 = createElement('h1', 'Als je op de muis klikt verandert deze tekst. Als je op klik hier klikt verandert de achtergrond van de canvas.');  
   
