9ad6e573bb2c899c00532e384f6382cf5dbce5e7
beas-site <58391367+beas-site@users.noreply.github.com>
2020-06-26 07:34:03 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -69,3 +69,15 @@ function mousePressed() {
   h1.html("Hieronder de winnende getallen:");
   createP("Het winnende getal is " + random(0,10) + ".");
 }
+function setup() {
+  createCanvas(100, 100, WEBGL);
+}
+
+function draw() {
+  background(0);
+  noFill();
+  stroke(100, 100, 240);
+  rotateX(frameCount * 0.01);
+  rotateY(frameCount * 0.01);
+  box(45, 45, 45);
+}
